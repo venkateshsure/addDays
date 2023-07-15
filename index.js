20f3d1992ddebeb13a754d2ae90dbe9c36205dd0
venkateshsure <venkatitzsure@gmail.com>
2023-07-15 16:14:18 +0000

    new change in add  days

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const addDays = require("date-fns/addDays");
+let days = 1;
 module.exports = function (days) {
-  const res = addDays(new Date(22, 7, 2020), days);
-  return `${res.getDate()}-${res.getMonth + 1}-${res.getFullYear()}`;
+  const res = addDays(new Date(2020, 7, 22), days);
+  return `${res.getDate()}-${res.getMonth() + 1}-${res.getFullYear()}`;
 };
+//const res = addDays(new Date(2020, 7, 22), 1);
+//console.log(res);
